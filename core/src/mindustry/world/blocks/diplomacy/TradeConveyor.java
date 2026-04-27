f9338012c62d649e3839ef3151c4e2ae1e572ed4
im1057inn <jackson.shev@gmail.com>
2026-04-27 15:41:33 +0300

    accept item bug fix

--- core/src/mindustry/world/blocks/diplomacy/TradeConveyor.java
+++ core/src/mindustry/world/blocks/diplomacy/TradeConveyor.java
@@ -16,24 +16,25 @@ public class TradeConveyor extends StackConveyor {
     public class TradeConveyorBuild extends StackConveyorBuild {
         @Override
         public boolean acceptItem(Building source, Item item){
-            if(this == source) return items.total() < itemCapacity && (!items.any() || items.has(item));
+            if(this == source) return items.total() < block.itemCapacity && (!items.any() || items.has(item));
             if(cooldown > recharge - 1f) return false;
-
-            if(source.team != team && !(source instanceof TradeConveyorBuild)) return false;
-            return items.total() < getMaximumAccepted(item) && (!items.any() || items.has(item));
+            if(source instanceof TradeConveyorBuild) {
+                return items.total() < block.itemCapacity && (!items.any() || items.has(item));
+            }
+            return items.total() < block.itemCapacity && (!items.any() || items.has(item));
         }
 
         @Override
         public void onProximityUpdate(){
             super.onProximityUpdate();
             Building forward = world.build(tile.x + Geometry.d4x[rotation], tile.y + Geometry.d4y[rotation]);
-
             if(forward instanceof TradeConveyorBuild){
                 state = stateMove;
-
-                if(!headless){
-                    blendprox |= 1;
-                }
+                if(!headless) blendprox |= 1;
+            } else if(forward != null && forward.block.acceptsItems){
+                state = stateUnload;
+            } else {
+                state = stateLoad;
             }
 
             Building back = world.build(tile.x - Geometry.d4x[rotation], tile.y - Geometry.d4y[rotation]);
@@ -42,22 +43,16 @@ public class TradeConveyor extends StackConveyor {
                 if(state == stateLoad) state = stateMove;
             }
         }
-
         @Override
         public void updateTile(){
             float eff = enabled ? (efficiency + baseEfficiency) : 1f;
-
             if(cooldown > 0f) cooldown = Mathf.clamp(cooldown - speed * eff * delta(), 0f, recharge);
-
             if(link == -1) return;
             if(cooldown > 0f) return;
-
             if(lastItem == null || !items.has(lastItem)){
                 lastItem = items.first();
             }
-
             if(!enabled) return;
-
             if(state == stateUnload){
                 while(lastItem != null && (!outputRouter ? moveForward(lastItem) : dump(lastItem))){
                     if(!outputRouter) items.remove(lastItem, 1);
@@ -70,17 +65,15 @@ public class TradeConveyor extends StackConveyor {
             }else{
                 if(state != stateLoad || (items.total() >= getMaximumAccepted(lastItem))){
                     Building forward = world.build(tile.x + Geometry.d4x[rotation], tile.y + Geometry.d4y[rotation]);
-
                     if(forward instanceof TradeConveyorBuild e && e.link == -1){
+                        link = forward.pos();
                         e.items.add(items);
                         e.lastItem = lastItem;
                         e.link = tile.pos();
-
-                        link = -1;
                         items.clear();
-
                         cooldown = recharge;
                         e.cooldown = 1f;
+                        link = -1;
                     }
                 }
             }
