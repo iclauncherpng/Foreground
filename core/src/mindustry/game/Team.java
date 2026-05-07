5a0ac1a37ec96dea846ef09418da087e4786c4ea
im1057inn <jackson.shev@gmail.com>
2026-05-07 20:27:59 +0300

    forerunners team changes

--- core/src/mindustry/game/Team.java
+++ core/src/mindustry/game/Team.java
@@ -48,7 +48,7 @@ public class Team extends mindustry.ctype.Content implements Senseable {
         sharded = new Team(1, "sharded", Pal.accent.cpy(), Color.valueOf("ffd37f"), Color.valueOf("eab678"), Color.valueOf("d4816b")),
         crux = new Team(2, "crux", Color.valueOf("f25555"), Color.valueOf("fc8e6c"), Color.valueOf("f25555"), Color.valueOf("a04553")),
         malis = new Team(3, "malis", Color.valueOf("a27ce5"), Color.valueOf("c7a4f5"), Color.valueOf("896fd6"), Color.valueOf("504cba")),
-        forerunners = new Team(4, "Forerunners", Color.valueOf("0d6307")),
+        forerunners = new Team(4, "forerunners", Color.valueOf("0d6307"), Color.valueOf("31a02d"), Color.valueOf("0d6307"), Color.valueOf("053103")),
 
         green = new Team(5, "green", Color.valueOf("54d67d")),//Color.valueOf("96f58c"), Color.valueOf("54d67d"), Color.valueOf("28785c")),
         blue = new Team(6, "blue", Color.valueOf("6c87fd")), //Color.valueOf("85caf9"), Color.valueOf("6c87fd"), Color.valueOf("3b3392")
