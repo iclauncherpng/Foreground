92ffcd55b3eef1e3953affda798c61cb56bef6e2
im1057inn <jackson.shev@gmail.com>
2026-05-08 16:42:09 +0300

    territory opacity by default is now 15%

--- core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -420,7 +420,7 @@ public class SettingsMenuDialog extends BaseDialog{
 
         graphics.sliderPref("unitlaseropacity", 100, 0, 100, 5, s -> s + "%");
         graphics.sliderPref("bridgeopacity", 100, 0, 100, 5, s -> s + "%");
-        graphics.sliderPref("territoryopacity", 100, 0, 100, 5, s -> s + "%");
+        graphics.sliderPref("territoryopacity", 15, 0, 50, 5, s -> s + "%");
 
         graphics.sliderPref("maxmagnificationmultiplierpercent", 100, 100, 200, 25, s -> {
             if(ui.settings != null){
