9b7ddb3b34b9b3b5c918f0668daa12f0efcb2583
im1057inn <jackson.shev@gmail.com>
2026-06-19 18:05:53 +0300

    Version.java changes

--- core/src/mindustry/core/Version.java
+++ core/src/mindustry/core/Version.java
@@ -109,6 +109,6 @@ public class Version{
         }
         String hash = commitHash.equals("unknown") ? "" : " (" + commitHash + ")";
 
-        return result + hash;
+        return result + signString() + hash;
     }
 }
